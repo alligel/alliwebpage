97e7405034b097d9019f642ef1c0ce9a0771bf2f
Alligel Randel <alligel@Thanwarats-MacBook-Pro.local>
2021-04-27 10:56:28 +0700

    fixed img

--- src/components/ArtWorks.jsx
+++ src/components/ArtWorks.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import A01 from "/img/A01.jpg";
-import A02 from "/img/A02.jpg";
-import A03 from "/img/A03.jpg";
-import A14 from "/img/A14.jpg";
-import A06 from "/img/A06.jpg";
-import A09 from "/img/A09.jpg";
-import A11 from "/img/A11.jpg";
-import A12 from "/img/A12.jpg";
-import A13 from "/img/A13.jpg";
-import A05 from "/img/A05.jpg";
+import A01 from "../img/A01.jpg";
+import A02 from "../img/A02.jpg";
+import A03 from "../img/A03.jpg";
+import A14 from "../img/A14.jpg";
+import A06 from "../img/A06.jpg";
+import A09 from "../img/A09.jpg";
+import A11 from "../img/A11.jpg";
+import A12 from "../img/A12.jpg";
+import A13 from "../img/A13.jpg";
+import A05 from "../img/A05.jpg";
 
 export default function ArtWorks(){
   return (
