2cc9cfee1243e226377632b739c9f937989c6ddd
Alligel Randel <alligel@Thanwarats-MacBook-Pro.local>
2021-04-27 09:02:55 +0700

    a01

--- src/components/ArtWorks.jsx
+++ src/components/ArtWorks.jsx
@@ -18,7 +18,7 @@ export default function ArtWorks(){
             <div className="row">
               <div className="column">
                 <div className="content">
-                  <img className="works-img" src={A01} alt="alli-artwork"/>
+                  <img className="works-img" src="https://alligel.github.io/ArtWorks/A01.jpg" alt="alli-artwork"/>
                 </div>
               </div>
 
