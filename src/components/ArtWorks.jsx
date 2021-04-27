9319e6ee9fc843c737452e063dccfeef948a91b6
Alligel Randel <alligel@Thanwarats-MacBook-Pro.local>
2021-04-27 08:50:50 +0700

    move img to components

--- src/components/ArtWorks.jsx
+++ src/components/ArtWorks.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import A01 from "./A01.jpg";
 import A02 from "./A02.jpg";
+import A03 from "./A03.jpg";
+import A14 from "./A14.jpg";
+import A07 from "./A07.jpg";
+import A09 from "./A09.jpg";
+import A11 from "./A11.jpg";
+import A12 from "./A12.jpg";
+import A13 from "./A13.jpg";
+import A05 from "./A05.jpg";
 
 export default function ArtWorks(){
   return (
@@ -9,7 +18,7 @@ export default function ArtWorks(){
             <div className="row">
               <div className="column">
                 <div className="content">
-                  <img className="works-img" src={process.env.PUBLIC_URL + '/images/A01.jpg'} alt="alli-artwork"/>
+                  <img className="works-img" src={A01} alt="alli-artwork"/>
                 </div>
               </div>
 
