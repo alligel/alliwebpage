fa9ebb9ef1655b8a29020e844badc11ec655b396
Alligel Randel <alligel@Thanwarats-MacBook-Pro.local>
2021-04-27 09:16:35 +0700

    relocate img folder

--- src/components/ArtWorks.jsx
+++ src/components/ArtWorks.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import A01 from "./A01.jpg";
-import A02 from "./A02.jpg";
-import A03 from "./A03.jpg";
-import A14 from "./A14.jpg";
-import A07 from "./A07.jpg";
-import A09 from "./A09.jpg";
-import A11 from "./A11.jpg";
-import A12 from "./A12.jpg";
-import A13 from "./A13.jpg";
-import A05 from "./A05.jpg";
+import A01 from "../images/A01.jpg";
+import A02 from "../images/A02.jpg";
+import A03 from "../images/A03.jpg";
+import A14 from "../images/A14.jpg";
+import A07 from "../images/A07.jpg";
+import A09 from "../images/A09.jpg";
+import A11 from "../images/A11.jpg";
+import A12 from "../images/A12.jpg";
+import A13 from "../images/A13.jpg";
+import A05 from "../images/A05.jpg";
 
 export default function ArtWorks(){
   return (
@@ -18,7 +18,7 @@ export default function ArtWorks(){
             <div className="row">
               <div className="column">
                 <div className="content">
-                  <img className="works-img" src="https://alligel.github.io/ArtWorks/A01.jpg" alt="alli-artwork"/>
+                  <img className="works-img" src={A01} alt="alli-artwork"/>
                 </div>
               </div>
 
