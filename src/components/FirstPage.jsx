a11a391594e8aec4badaada034626f1b7832b160
Alligel Randel <alligel@Thanwarats-MacBook-Pro.local>
2021-05-14 19:07:44 +0700

    addsplash

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import A07 from "./img/A07.jpg";
+import Abirdforsplash from "./img/Abirdforsplash.png"
 import NavBar from "./NavBar"
 import AboutMe from "./AboutMe"
 
@@ -7,11 +8,13 @@ export default function FirstPage() {
   
   return (
 <div class="top-container">
-    <div>
+    <div className="splashbg">
         <NavBar />
         <h1 className="nameText">Alli</h1>
+        <img alt="splash" className="birdsplash" src={Abirdforsplash}/>
     </div>
-    <div class="about-container">
+
+    <div className="about-container">
         <AboutMe />
     </div>
         <img alt="foxbg" className="image-bg" src={A07}/>
