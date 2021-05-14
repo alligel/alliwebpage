49b77d3b882c432eb52d4132a25a94c87997da0f
Alligel Randel <alligel@Thanwarats-MacBook-Pro.local>
2021-05-14 20:10:14 +0700

    edit splash

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import A07 from "./img/A07.jpg";
 import Splash from "./img/Splash.jpg";
+import SplashwithText from "./img/SplashwithText.jpg";
 import NavBar from "./NavBar"
 import AboutMe from "./AboutMe"
 
@@ -9,14 +10,14 @@ export default function FirstPage() {
   return (
 <div>
 
-<div class="top-container">
+<div className="top-container">
     <div>
         <NavBar />
         <h1 className="nameText">Alli</h1>
-        <img id="home" alt="splash" className="splash-bg" src={Splash}/>
+        <img id="home" alt="splash" className="splash-bg" src={SplashwithText}/>
     </div>
 </div>
-<div class="top-container">
+<div className="top-container">
     <div className="about-container">
         <AboutMe />
     </div>
