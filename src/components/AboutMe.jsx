e9605df6c6fefed9ffd4b29eca7fd23a28777d76
Alligel Randel <alligel@Thanwarats-MacBook-Pro.local>
2021-05-15 16:35:22 +0700

    about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,17 +9,17 @@ export default function AboutMe(){
     <div className="top-container">
       {/* profile */}
       <div className="about-container">
-         <div>
+         {/* <div> */}
             <h1  id="aboutme">About Me</h1>
             <img alt="myprofile" className="profilepic" src={Profileimg}/>
-         </div>
-         <div>
+         {/* </div> */}
+         {/* <div> */}
             <h4>After spending time learning and doing new things,</h4>
             <h4>I found my passion in UX/UI design and coding, I want to learn more about it!</h4>
             <h4>I wish to become a great UX/UI designer one day.</h4>
             <br></br>
             <hr className="hr-style"></hr>
-         </div>
+         {/* </div> */}
 
          {/* education */}
 
