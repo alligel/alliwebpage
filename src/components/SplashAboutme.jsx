58f8ad382ada179ff447530517d069d6c1b8b50a
Alligel Randel <alligel@Thanwarats-MacBook-Pro.local>
2021-05-15 19:47:48 +0700

    splash border

--- src/components/SplashAboutme.jsx
+++ src/components/SplashAboutme.jsx
@@ -14,7 +14,9 @@ export default function SplashAboutme() {
     <div>
         <NavBar />
         <div id="home" className="splash-bg">
+        <div className="slash-line">
         <img alt="bird" className="splash-bird" src={SplashNobg}/>
+        </div>
         </div>
     </div>
         
--- src/components/WhatIDo.jsx
+++ src/components/WhatIDo.jsx
@@ -11,7 +11,7 @@ export default function WhatIDo(){
          <div className="white-container">
              <div className="row">
 
-                 <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6 whatido-container">
+                 <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4 whatido-container">
                      <i className="fas fa-paint-brush skill-icon"></i>
                      <h2>Art & Design</h2>
                      <p>Digital painting is always one of my favorite hobbies.
@@ -22,7 +22,7 @@ export default function WhatIDo(){
                 </div>
                     
 
-                 <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6 whatido-container">
+                 <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4 whatido-container">
                      <i className="fas fa-code skill-icon"></i>
                      <h2>Coding</h2>
                      <p>Although I'm a amateur in this area, I'm trying my best here.
@@ -31,7 +31,7 @@ export default function WhatIDo(){
                      and to be able to change the subject above from "Coding" to "Programming".</p>     
                  </div>
 
-                  <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6 whatido-container">
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4 whatido-container">
                   <i className="fas fa-camera-retro skill-icon"></i>
                     <h2>Photography & <br/>Video Editing</h2>
                     <p>When having a group project, the responsibility of taking and editing both photo and video has always been given to me
