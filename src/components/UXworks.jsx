c859c7d7c18ee7177a5f5fb45e51a3900eb5c1ba
Alligel Randel <alligel@Thanwarats-MacBook-Pro.local>
2021-05-18 22:12:10 +0700

    add space button

--- src/components/UXworks.jsx
+++ src/components/UXworks.jsx
@@ -11,8 +11,8 @@ function UXworks() {
         <img className="ux-img" src={NBLads} alt="alli-uxwork"/>
         <br/>
         <br/>
-        <p>For NBL project, I made a prototype of how to use the "BORROW" function<button className="btn-link"><a href="https://alli38803.invisionapp.com/console/share/MB2KWCUZQ5/630036371" target="_blank">NBL Prototype</a></button></p>
-        <p>More information about the NBL project<button className="btn-link"><a href="https://drive.google.com/file/d/1I-zUG4Z1Nm-3kb20z1cO6z9YE7tIfzE8/view?usp=sharing" target="_blank">PDF File</a></button></p>
+        <p>For NBL project, I made a prototype of how to use the "BORROW" function  <button className="btn-link"><a href="https://alli38803.invisionapp.com/console/share/MB2KWCUZQ5/630036371" target="_blank">NBL Prototype</a></button></p>
+        <p>More information about the NBL project  <button className="btn-link"><a href="https://drive.google.com/file/d/1I-zUG4Z1Nm-3kb20z1cO6z9YE7tIfzE8/view?usp=sharing" target="_blank">PDF File</a></button></p>
         <br/>
         <br/>
         </div>
